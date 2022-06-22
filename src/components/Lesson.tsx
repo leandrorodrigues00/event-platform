54a10f64a01ee0423bed622ff36135c05fd947d7
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-06-21 21:23:55 -0300

    aula 2 - estrutura visual

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,14 @@
+export function Lesson() {
+  return (
+    <a href="#">
+      <span className="text-gray-300">Segunda • 21 de junho • 19h00</span>
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header>
+          <span>Conteúdo Liberado</span>
+          <span>AO VIO</span>
+        </header>
+        <strong>Abertura do evento Ignite Lab</strong>
+      </div>
+    </a>
+  );
+}
