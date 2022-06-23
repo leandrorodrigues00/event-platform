43800df05b092429f8247721dbb53252b0d3037f
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-06-22 21:44:44 -0300

    aula 3!

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ interface GetLessonsQueryResponse {
 }
 export function Sidebar() {
   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
-  console.log(data);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
