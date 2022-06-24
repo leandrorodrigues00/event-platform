6e5fcc156c3ef4988210dab26be28c3e410cb0d7
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2022-06-23 22:12:21 -0300

    aula 4 completa

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4opb3v50lbe01xxgg96b2ie/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
